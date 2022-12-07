7e6f7c1b6d2e5bea779d142389baeebd49931f54
이주현 <leejoohyun@ijuhyeon-ui-MacBookPro.local>
2022-12-07 16:29:25 +0900

    image databinding

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -4,7 +4,7 @@
       <div class="profile"></div>
       <span class="profile-name">{{게시물.name}}</span>
     </div>
-    <div class="post-body"></div>
+    <div class="post-body" :style="{backgroundImage : `url(${게시물.postImage})`}"></div>
     <div class="post-content">
       <p>{{게시물.likes}} Likes</p>
       <p><strong>{{게시물.name}}</strong> {{게시물.content}}</p>
